c2914a198535df1e6d43b47b425e968073be0a7f
Georgy <georgy.tertychniy@yandex.ru>
2026-02-15 17:30:42 +0300

    unit base class and scriptable object

---
+++ Assets/Scripts/ScriptableObjects/UnitObject.cs
@@ -0,0 +1,43 @@
+using System;
+using Unity.VisualScripting;
+using UnityEngine;
+
+namespace Units
+{
+    [CreateAssetMenu(fileName = "BaseUnit", menuName = "Units/GameSettingsScriptableObject", order = 2)]
+    public class BaseUnitObject: ScriptableObject, IUnit
+    {
+
+        [field: SerializeField]  public int Health { get; set; }
+
+        [field: SerializeField]public int Attack {get; set;}
+
+        [field: SerializeField]public float Speed {get; set;}
+
+        [field: SerializeField]public float AttackRange {get; set;}
+        [field: SerializeField]public int Armor {get; set;}
+
+        [field: SerializeField]public float AttackCooldown {get; set;}
+
+        [field: SerializeField] public UnitType Type {get; set;}
+        public void AttackTarget(IUnit target)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Die()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void MoveTo(float x, float y)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void TakeDamage(int damage)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ Assets/Scripts/Units/BaseUnit.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using UnityEngine;
+
+namespace Units
+{
+    public class BaseUnit : MonoBehaviour, IUnit
+    {
+
+        [SerializeField] protected BaseUnitObject unitObject;
+
+        public virtual int Health { get => unitObject.Health; protected set => unitObject.Health = value; }
+        public virtual int Attack => unitObject.Attack;
+        public virtual float Speed => unitObject.Speed;
+        public virtual float AttackRange => unitObject.AttackRange;
+
+        public virtual int Armor => unitObject.Armor;
+        public virtual float AttackCooldown => unitObject.AttackCooldown;
+        public virtual UnitType Type => unitObject.Type;
+
+        public virtual void TakeDamage(int damage)
+        {
+            Health -= damage;
+            if (Health <= 0)
+            {
+                Die();
+            }
+        }
+
+        public virtual void Die()
+        {
+            // Handle death logic here (e.g., play animation, remove from game, etc.)
+        }
+
+        public void AttackTarget(IUnit target)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void MoveTo(float x, float y)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private void Start()
+        {
+            // Initialize unit properties here
+        }
+
+        private void Update()
+        {
+            // Handle unit behavior here (e.g., movement, attacking, etc.)
+        }
+
+    }
+}
