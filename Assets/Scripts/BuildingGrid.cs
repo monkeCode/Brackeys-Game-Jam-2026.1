bda166cb395fef2c06021597c1ced4a309900df8
Алексей Хоченков <alekshoch@yandex.ru>
2026-02-16 20:59:55 +0300

    smth

--- Assets/Scripts/BuildingGrid.cs
+++ Assets/Scripts/BuildingGrid.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class BuildingGrid : MonoBehaviour
 {
-    [SerializeField] public Vector2Int GridSize = new Vector2Int(20, 10);
+    [SerializeField] public Vector2Int GridSize = new(20, 10);
     private IBuilding[,] grid;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
