35bfe09dd6ee14b1d438d528dad0d63771199377
Georgy <georgy.tertychniy@yandex.ru>
2026-02-16 20:16:47 +0300

    add command to damageable

--- Assets/Interfaces/IDamageable.cs
+++ Assets/Interfaces/IDamageable.cs
@@ -1,8 +1,16 @@
 
 
+public enum Command
+{
+    Player,
+    Enemy
+}
+
 public interface IDamageable 
 {
     public int Health { get; }
+
+    public Command Command {get; }
 
     public void TakeDamage(int damage);
 }
